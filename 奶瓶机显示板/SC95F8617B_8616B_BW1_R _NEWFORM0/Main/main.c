17727f39031fbce02e20de2397c6fe9e1fbd47fa
LEOlianggaosang <143078356+LEOlianggaosang@users.noreply.github.com>
2026-07-04 18:39:39 +0800

    ✨feat：功能(增加样机调试功能): 版本0
    2026-07-02
    
    !!!BREAKING CHANGE:

--- 奶瓶机显示板/SC95F8617B_8616B_BW1_R _NEWFORM0/Main/main.c
+++ 奶瓶机显示板/SC95F8617B_8616B_BW1_R _NEWFORM0/Main/main.c
@@ -31,6 +31,12 @@ void  main(void)
   Recovery_Power_Lost_Memory(CONFIG_MENORYENABLE);
   //LOG_PRINTF("Info:Enter working loop!\n");
 	//Work_NextStepHandler  = Work_NextStep;//厂测模式不在开始启动
+
+  #if CONFIG_POWER_TEST//20260702
+  F_Led_Power = 1;
+	Com1_Buf = Com2_Buf = Com3_Buf = DISP_CHAR_NULL;
+  #endif
+
   /* System Working loop */  
   #if CONFIG_TK_DEBUG
 	SOCAPI_DeBugTouchKey_Init();
@@ -65,12 +71,77 @@ void  main(void)
 
 #if CONFIG_POWER_TEST//20260509
 static void Work_PowerTest(void)
-{
+{//负载驱动规则
 	#if NEWTPE1
-  Output_IP = Inlet_IsInLetting = F_Led_SelfClean;
-  FlowMCntSet = 0xffff;
-	#endif
-	Relay_DP = F_Led_Fast;
-  Output_WP = F_Led_Srandard;
+  if(F_Led_Power||F_Led_Start)
+  {
+    RelayEnable = RelayEnable =0;
+    Inlet_IsInLetting = 0;
+    FlowMCntSet = 0;
+    Inlet_ErrReload = 1;
+  }
+  else
+  {
+    if(F_Led_Light)
+    {//对流量、温度、转速控制作处理20260702
+      RelayEnable = RelayEnable =0;
+      Inlet_IsInLetting = 0;
+      FlowMCntSet = 0;
+      if(F_Led_SelfClean)
+      {//进水800ml
+        FlowMCntSet = 8*C_LITER;
+        if(P_FlowMCnt<=FlowMCntSet)
+        {
+          Output_IP = Inlet_IsInLetting = 1;
+        }
+      }
+
+      if(F_Led_Fast)
+      {//排水箱
+        Relay_DP = Output_IP = Inlet_IsInLetting = 1;
+        FlowMCntSet = 0xffff;
+      }
+
+      if(F_Led_Srandard)
+      {//加热洗涤到设定温度一次
+        Output_WP = 1;
+        if((Temperature_Value<=45)&&(!AdErr_ErrRe))
+        {
+          Relay_HEAT = 1;
+        }
+      }
+
+      if(F_Led_Sterilize)
+      {//进水250ml
+        FlowMCntSet = (C_LITER*25)/10;
+        if(P_FlowMCnt<=FlowMCntSet)
+        {
+          Output_IP = Inlet_IsInLetting = 1;
+        }
+      }
+
+      if(F_Led_Save)
+      {//PTC与臭氧检测转速
+        Output_FAN_IN = 1;
+        if(P_FeedBackCnt >= 50)
+        {
+          Relay_PTC = 1;
+          #if CONFIG_O3_ENABLE
+          Output_O3 = 1;
+          #endif
+        }
+      }
+    }
+    else
+    {
+      Output_IP = Inlet_IsInLetting = F_Led_SelfClean;
+      FlowMCntSet = 0xffff;
+      Relay_DP = F_Led_Fast;
+      Output_WP = F_Led_Srandard;
+      Relay_HEAT = F_Led_Sterilize;
+      Output_FAN_IN = F_Led_Save;
+    }
+  }
+  #endif
 }
 #endif
